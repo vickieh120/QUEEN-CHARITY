1650f150767681fd81986e2f17fdb01e6399dc25
caseyweb <caseywebsthedonn25@gmail.com>
2024-10-31 12:26:12 +0300

    Update _extra.js

--- plugins/_extra.js
+++ plugins/_extra.js
@@ -184,7 +184,7 @@ smd({
         edit: _0x2d7cf6
       }, "asta", _0x4e7c63);
     } catch (_0x28ce7e) {}
-    let _0x50f17a = " *👸 Q U E E N  M A R I A- ＢＯＴ ＦＥＡＴＵＲＥ*\n\n\n  ◦ _Total Features👑 " + _0x4cf8ed + "_\n  \n*◦ BOT FEATURES*\n\n      Plugins👑 " + Object.values(_0x4de967.commands).filter(_0x54d4bf => _0x54d4bf.pattern).length + "_\n      _Msg Listener👑 " + Object.values(_0x4de967.commands).filter(_0x2376a3 => _0x2376a3.on).length + "_\n      _Call Listener 👑 " + Object.values(_0x4de967.commands).filter(_0x54a19b => _0x54a19b.call).length + "_\n      _Group Listener 👑 " + Object.values(_0x4de967.commands).filter(_0x35381c => _0x35381c.group).length + "_\n  \n\n" + Config.caption;
+    let _0x50f17a = " *👸 Q U E E N  C H A R I T Y- ＢＯＴ ＦＥＡＴＵＲＥ*\n\n\n  ◦ _Total Features👑 " + _0x4cf8ed + "_\n  \n*◦ BOT FEATURES*\n\n      Plugins👑 " + Object.values(_0x4de967.commands).filter(_0x54d4bf => _0x54d4bf.pattern).length + "_\n      _Msg Listener👑 " + Object.values(_0x4de967.commands).filter(_0x2376a3 => _0x2376a3.on).length + "_\n      _Call Listener 👑 " + Object.values(_0x4de967.commands).filter(_0x54a19b => _0x54a19b.call).length + "_\n      _Group Listener 👑 " + Object.values(_0x4de967.commands).filter(_0x35381c => _0x35381c.group).length + "_\n  \n\n" + Config.caption;
     await _0x4e7c63.bot.relayMessage(_0x4e7c63.chat, {
       requestPaymentMessage: {
         currencyCodeIso4217: "PK",
